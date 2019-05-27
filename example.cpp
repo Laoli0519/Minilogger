f32f76ad35ce3771f73a29810ecd0450b395cc33
Laoli0519 <1587791925@qq.com>
2019-05-27 14:20:39 +0800

    Added two output methods

--- example.cpp
+++ example.cpp
@@ -2,39 +2,32 @@
 #include<iostream>
 #include<string>
 
-using namespace std;
-
-#define LOG(fmt, args...)	do{\
-			char _buf[1024] = {0};\
-			snprintf(_buf, sizeof(_buf), "[%s:%s:%d][LOG_NORMAL]"fmt"\n",__FILE__,__FUNCTION__,__LINE__, ##args);\
-			mylog.Log(string(_buf));\
-		}while(false)
-
-
-
-//you can define your own Textdecorator and use it.
-Logger<TextDecorator> mylog("mylogfile.txt","this is title!",true,true);
-
 void myfunction(){
-    //test LOG in function
-    //
     int a = 1;
     int b = 2;
+    char arr1[] = "Hello World";
+    char arr2[] = "MiniLogger!";
 
-    LOG("this is a log in function,a+b=%d",a+b);
+
+    MiniLog::Logger *logger = new MiniLog::Logger("myfunction", MiniLog::log_level_info);
+    logger->info("hello world");
+    logger->error("a = {}, b = {}, arr1[0] = {}, arr1 = {}, arr2 = {}.", a, b, arr1[0], arr1, arr2);
+
+    //will not print anything
+    logger->debug("a = {}, b = {}, arr1[0] = {}, arr1 = {}, arr2 = {}.", a, b, arr1[0], arr1, arr2);
+
+    //The following operations will be output to the file.
+    MiniLog::Logger *logger2 = new MiniLog::Logger("C:/tmp/log.txt", "main", MiniLog::log_level_debug);
+    logger2->info("hello world");
+    logger2->debug("...");
+
 }
 
 
 //this is some example for using minilogger
 int main(){
-    //log with no args.
-    mylog.Log("this is log.");
-	std::cout << "do something" << std::endl;
-    // log with args and function name and line number.
-	LOG("test for LOG,args: number:[%d], string:[%s]",1,"yoyoyo");
 
     myfunction();
-	std::cin.get();
     return 0;
 
 }
--- minilogger.h
+++ minilogger.h
@@ -1,178 +1,294 @@
 //Easy-to-use log classes,called minilogger
+//Requires C++11 standard and above
+
+
 #ifndef LOGGER_H
 #define LOGGER_H
 #include <string>
 #include <fstream>
-
+#include <iostream>
+#include <sstream>
+#include <initializer_list>
 // ============================================================
 //  get system time 
 // ============================================================
-#include <time.h>
+#ifdef WIN32
+	#include <Windows.h>
+#elif linux
+	#include <time.h>
+#endif
 
 
 // ============================================================
-// time in 24 hours hh:mm:ss format
+// time in 24 hours YYYY:MM:DD hh:mm:ss format
 // ============================================================
-static std::string TimeStamp()
+static std::string DateTimeStamp()
 {
-	char str[9];
+	char str[64];
 
 	// get the time, and convert it to struct tm format
+#ifdef WIN32
+	SYSTEMTIME currentTime;
+	GetLocalTime(&currentTime);
+
+	sprintf_s(str, "[%u-%u-%u %d:%u:%u.%u]", currentTime.wYear, currentTime.wMonth, \
+		currentTime.wDay, currentTime.wHour, \
+		currentTime.wMinute, currentTime.wSecond, \
+		currentTime.wMilliseconds);
+#elif defined(linux)
 	time_t a = time(0);
 	struct tm* b = localtime(&a);
 
 	// print the time to the string
-	strftime(str, 9, "%H:%M:%S", b);
-
+	strftime(str, 64, "%Y-%m-%d %H:%M:%S", b);
+#endif
 	return str;
 }
 
-// ============================================================
-// date YYYY:MM:DD format
-// ============================================================
-static std::string DateStamp()
-{
-	char str[11];
+namespace MiniLog {
+	const uint16_t log_level_debug = 1;
+	const uint16_t log_level_info = 2;
+	const uint16_t log_level_warning = 4;
+	const uint16_t log_level_error = 8;
+	const uint16_t log_level_fatal = 16;
 
-	// get the time, and convert it to struct tm format
-	time_t a = time(0);
-	struct tm* b = localtime(&a);
+	// ============================================================
+	// New Logger with a new log file and new log title
+	// ============================================================
+	class Logger {
+		public:
+			Logger(const std::string p_filename,
+				const std::string p_username,
+				const uint16_t log_level = MiniLog::log_level_debug); 
+			
+			Logger(const std::string p_username, 
+				const uint16_t log_level = MiniLog::log_level_debug); 
 
-	// print the time to the string
-	strftime(str, 11, "%Y.%m.%d", b);
+			~Logger();
 
-	return str;
+		public:
+
+			void set_log_level(const uint16_t level);
+
+			template <typename T>
+			void log(const T& fmt) ;
+
+			template <typename T, typename... Args >
+			void log(const char *fmt, const T& value, const Args &... args) ;
+
+			template<typename... Args>
+			void debug(const char *fmt, const Args &... args);
+
+			template<typename... Args>
+			void info(const char *fmt, const Args &... args);
+
+			template<typename... Args>
+			void warning(const char *fmt, const Args &... args);
+
+			template<typename... Args>
+			void error(const char *fmt, const Args &... args);
+
+			template<typename... Args>
+			void fatal(const char *fmt, const Args &... args);
+
+			template<typename T>
+			void debug(const T & fmt);
+
+			template<typename T>
+			void info(const T & fmt);
+
+			template<typename T>
+			void warning(const T & fmt);
+
+			template<typename T>
+			void error(const T & fmt);
+
+			template<typename T>
+			void fatal(const T & fmt);
+
+			void flush();
+		private:
+			bool should_log(const uint16_t level);
+
+		protected:
+			std::ofstream 		m_logfile;
+			const uint16_t 		m_log_level;
+			const std::string 	m_username;
+			std::ostream		m_out_stream;
+	};
+
+	
+	Logger::Logger(const std::string p_filename,
+		const std::string p_username,
+		const uint16_t log_level):
+			m_log_level(log_level), 
+			m_username(p_username), 
+			m_out_stream(std::cout.rdbuf()) {
+		// now the tricky part... testing to see if a file is open or not.
+	    // stupid C++. You need to open a file in read mode, and if it doesn't
+	    // open correctly, you know that it doesn't exist.
+		// a file is open or not.
+		std::fstream filetester(p_filename.c_str(), std::ios::in);
+
+		if (filetester.is_open()) {
+			filetester.close();
+			m_logfile.open(p_filename.c_str(), std::ios::out | std::ios::app | std::ios::binary);
+		}
+		else {
+			m_logfile.open(p_filename.c_str(), std::ios::out | std::ios::binary);
+		}
+		std::streambuf *buf = m_logfile.rdbuf();
+		m_out_stream.rdbuf(buf);
+	}
+
+	Logger::Logger(const std::string p_username, const uint16_t log_level):
+			m_log_level(log_level),
+			m_username(p_username),
+			m_out_stream(std::cout.rdbuf()) {}
+
+	Logger::~Logger() {
+		if (m_logfile.is_open()) {
+			m_logfile.close();
+		}
+	}
+
+	inline bool Logger::should_log(const uint16_t level) {
+		return level >= m_log_level;	
+	}
+
+	template <typename T, typename... Args >
+	void Logger::log(const char *fmt, const T& value, const Args &... args) {
+		for(int i = 0; '\0' != fmt[i]; ++i) {
+			if('{' == fmt[i] && '}' == fmt[i + 1]) {
+				m_out_stream << value;
+				log(fmt + 2 + i, args...);	
+				break;
+			}
+			else {
+				m_out_stream << fmt[i];
+			}
+		}
+	}
+
+	template <typename T>
+	void Logger::log(const T& fmt) {
+		m_out_stream << fmt << std::endl;
+	}
+
+	template<typename... Args>
+	void Logger::debug(const char *fmt, const Args &... args) {
+		if(!should_log(MiniLog::log_level_debug)) {
+			return ;
+		}
+
+		std::string message;
+		message = DateTimeStamp() + " [" + m_username + "] [debug] " + fmt;
+		log(message.c_str(), args...);
+	}
+
+	template<typename... Args>
+	void Logger::info(const char *fmt, const Args &... args) {
+		if(!should_log(MiniLog::log_level_info)) {
+			return ;
+		}
+
+		std::string message;
+		message = DateTimeStamp() + " [" + m_username + "] [info] " + fmt;
+		log(message.c_str(), args...);
+	}
+
+	template<typename... Args>
+	void Logger::warning(const char *fmt, const Args &... args) {
+		if(!should_log(MiniLog::log_level_warning)) {
+			return ;
+		}
+
+		std::string message;
+		message = DateTimeStamp() + " [" + m_username + "] [warning] " + fmt;
+		log(message.c_str(), args...);
+	}
+
+	template<typename... Args>
+	void Logger::error(const char *fmt, const Args &... args) {
+		if(!should_log(MiniLog::log_level_error)) {
+			return ;
+		}
+
+		std::string message;
+
+		message = DateTimeStamp() + " [" + m_username + "] [error] " + fmt;
+		log(message.c_str(), args...);
+	}
+
+	template<typename... Args>
+	void Logger::fatal(const char *fmt, const Args &... args) {
+		if(!should_log(MiniLog::log_level_fatal)) {
+			return ;
+		}
+
+		std::string message;
+		message = DateTimeStamp() + " [" + m_username + "] [fatal] " + fmt;
+		log(message.c_str(), args...);	
+	}
+
+	template<typename T>
+	void Logger::debug(const T & fmt) {
+		if (!should_log(MiniLog::log_level_debug)) {
+			return;
+		}
+
+		std::stringstream message;
+		message << DateTimeStamp() << " [" + m_username + "] [debug] " + fmt;
+		log(message.str().c_str());
+	}
+
+	template<typename T>
+	void Logger::info(const T & fmt) {
+		if (!should_log(MiniLog::log_level_info)) {
+			return;
+		}
+
+		std::stringstream message;
+		message << DateTimeStamp() << " [" << m_username << "] [info] " << fmt;
+		log(message.str().c_str());
+	}
+
+	template<typename T>
+	void Logger::warning(const T & fmt) {
+		if (!should_log(MiniLog::log_level_warning)) {
+			return;
+		}
+
+		std::stringstream message;
+		message << DateTimeStamp() << " [" << m_username << "] [warning] " << fmt;
+		log(message.str().c_str());
+	}
+
+	template<typename T>
+	void Logger::error(const T & fmt) {
+		if (!should_log(MiniLog::log_level_error)) {
+			return;
+		}
+
+		std::stringstream message;
+		message << DateTimeStamp() << " [" << m_username << "] [error] " << fmt;
+		log(message.str().c_str());
+	}
+
+	template<typename T>
+	void Logger::fatal(const T & fmt) {
+		if (!should_log(MiniLog::log_level_fatal)) {
+			return;
+		}
+
+		std::stringstream message;
+		message << DateTimeStamp() << " [" << m_username << "] [fatal] " << fmt;
+		log(message.str().c_str());
+	}
+
+	inline void Logger::flush() {
+		m_out_stream.flush();
+	}
 }
-
-
-// ============================================================
-// Here is an example of a simple log decorator, you can define your own decorator
-// ============================================================
-class TextDecorator
-{
-public:
-	static std::string FileHeader(const std::string& p_title)
-	{
-		return "==================================================\n" +
-			p_title + "\n" +
-			"==================================================\n\n";
-	}
-
-	static std::string SessionOpen()
-	{
-		return "\n";
-	}
-
-	static std::string SessionClose()
-	{
-		return "\n";
-	}
-
-	static std::string Decorate(const std::string& p_string)
-	{
-		return p_string + "\n";
-	}
-};
-
-
-// ============================================================
-// New Logger with a new log file and new log title
-// ============================================================
-template<class decorator>
-class Logger
-{
-public:
-	Logger(const std::string& p_filename,
-		const std::string& p_logtitle,
-		bool p_timestamp = false,
-		bool p_datestamp = false);
-
-	~Logger();
-	void Log(const std::string& p_entry);
-
-protected:
-	std::fstream m_logfile;
-	bool m_timestamp;
-	bool m_datestamp;
-
-};
-
-
-typedef Logger<TextDecorator> TextLog;
-
-
-
-template<class decorator>
-Logger<decorator>::Logger(const std::string& p_filename,
-	const std::string& p_logtitle,
-	bool p_timestamp,
-	bool p_datestamp)
-{
-	// now the tricky part... testing to see if a file is open or not.
-    // stupid C++. You need to open a file in read mode, and if it doesn't
-    // open correctly, you know that it doesn't exist.
-	// a file is open or not.
-	std::fstream filetester(p_filename.c_str(), std::ios::in);
-
-	if (filetester.is_open())
-	{
-		// the file exists, so just close the test file
-		filetester.close();
-
-		// open the real file and set app mode
-		m_logfile.open(p_filename.c_str(), std::ios::out | std::ios::app);
-	}
-	else
-	{
-		// file doesn't exist.
-		m_logfile.open(p_filename.c_str(), std::ios::out);
-
-		// print out a file header to the file
-		m_logfile << decorator::FileHeader(p_logtitle);
-	}
-
-	// print out an opening statement. Make sure it is time-and-date-stamped
-	m_timestamp = true;
-	m_datestamp = true;
-	m_logfile << decorator::SessionOpen();
-	Log("Session opened.");
-	m_timestamp = p_timestamp;
-	m_datestamp = p_datestamp;
-
-}
-
-template< class decorator >
-Logger< decorator >::~Logger()
-{
-	m_timestamp = true;
-	m_datestamp = true;
-	Log("Session closed.");
-	m_logfile << decorator::SessionClose();
-
-}
-
-
-template< class decorator >
-void Logger< decorator >::Log(const std::string& p_entry)
-{
-	std::string message;
-
-	if (m_datestamp)
-	{
-		message += "[" + DateStamp() + "] ";
-	}
-	if (m_timestamp)
-	{
-		message += "[" + TimeStamp() + "] ";
-	}
-
-	message += p_entry;
-	m_logfile << decorator::Decorate(message);
-	m_logfile.flush();
-}
-
-
-
-
 
 #endif
